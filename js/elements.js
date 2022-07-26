89ab7063c689e21547c017acfaddc49eaa3ff59e
carmmcl <carolina.garcia@mail.com>
2022-07-26 12:45:28 -0300

    modificaciones

--- js/elements.js
+++ js/elements.js
@@ -1,25 +1,23 @@
 //Se agregará un nuevo elemento (p) al HTML. 
-
 //Se lista el párrafo del HTML en la consola
-
-let pars= document.getElementsByTagName("p");
-
-console.log (pars[0].innerHTML);
-
-
+/*let pars= document.getElementsByTagName("p");
+console.log (pars[0].innerHTML);*/
+/*
 //se crea el nuevo parrafo y se refleja en el HTML
-
-let big = document.getElementById ("parr");
-
+let divparr = document.getElementById ("parr");
 let parrafos = ["Para obtener el promedio, ingrese los datos a continuación"];
 
 for (const parrafo of parrafos) {
     let p = document.createElement("p");
     p.innerHTML = parrafo
-    big.appendChild (p);
-
+    divparr.appendChild (p);
 }
-//Se listan los parrafos 
 
+//Se listan los parrafos en la consola
 console.log (pars[0].innerHTML);
 console.log (pars[1].innerHTML);
+*/
+let divparr = document.getElementById ("parr");
+let newParagraph=document.createElement("p");
+newParagraph.innerText="Para obtener el promedio, ingrese los datos a continuación";
+divparr.append(newParagraph);
--- js/events.js
+++ js/events.js
@@ -1,82 +1,93 @@
 
-
-let mostrar = document.getElementById("mostrar");
-let nombre = document.getElementById("inputName");
-let apellido = document.getElementById ("inputLastName");
-let nota1 =document.getElementById("nota1");
-const datos =document.getElementById ("datos");
-
-mostrar.addEventListener ("click", ()=>{
-
-    const data = {
-        nombre:nombre.value,
-        apellido:apellido.value,
-        nota1:nota1.value,
-    }
-
-    fetch ('https://jsonplaceholder.typicode.com/posts',{
-        method:'POST',
-        body:JSON.stringify(data),
-       headers:{
-        "Content-type":"application/json; charset=UTF-8",
-    }
-     })
-
-    .then ((response) => response.json())
-    .then ((data) => {
-        console.log(data);
-        datos.innerHTML =  `<p>${data.nombre}</p>
-                            <p>${data.apellido}</p>
-                            <p>${data.nota1}</p>
-                            <p> Datos enviados exitosamente</p>`
-
-
-    })
-});
-
-
-
-
-//Se incorpora un evento
-/*
+//Incorporación de evento, se utilizan funciones, condicionales, sintaxis avanzada  y una librería relevante al proyecto
 document.getElementById("button1").addEventListener("click", validarCargaDatos);
-let message = document.getElementById ("message");
-
+let mostrar = document.getElementById("mostrar");
+let mostrar2 = document.getElementById("mostrar2");
+const datos =document.getElementById ("datos");
 
 function validarCargaDatos(e) {
     e.preventDefault();
+    let id = document.getElementById("inputId").value;
     let nombre = document.getElementById("inputName").value;
     let apellido = document.getElementById ("inputLastName").value;
     let nota1 =document.getElementById("nota1").value;
-    
-    ((nombre.length <3 )  ||  (apellido.length <3 )|| (nota1==""))?
-
+    let nota2 =document.getElementById("nota2").value;
+    let sumarNotas = parseInt(nota1)+parseInt(nota2);
+    let listaEstudiantes=document.getElementById("students");
+    let estudiantes=[];
+        
+    ((id=="") || (nombre.length <3 )  ||  (apellido.length <3 )|| (nota1=="")|| (nota2==""))?
     
     Swal.fire ({
     title:`error`,
-    text:`El nombre  y el apellido deben contener 3 caracteres como mínimo, el campo de nota deben ser completados`,
+    text:`El nombre  y el apellido deben contener 3 caracteres como mínimo, los campos de id y notas deben ser completados`,
     icon:`error`,
+    }):
 
-}):
-Swal.fire ({
+    Swal.fire ({
     title:`Excelente!`,
     text:`Los datos han sido ingresados correctamente`,
     icon:`success`,
-});
-  
-        localStorage.setItem(nombre, nota1)
+    });
 
-
-
-
+    function calcularPromedio (){
+         return sumarNotas/2
     }
+     
+ //Luego de ingresar los dtaos solicitados, se muestra la información relevante al usuario   
+    mostrar.addEventListener ("click",()=>{
+    
+        let newStudent=document.createElement("li");
+        let promedio=calcularPromedio();
+        newStudent.innerHTML= `<p>${nombre} ${apellido} - Promedio:${promedio}</p>`;                              
+        
+        listaEstudiantes.append(newStudent);
+    });
 
-*/
-
-
+//Se construye el objeto  
+   let dataEstudiante;
+   dataEstudiante = {
+    id:id,
+    nombre:nombre,
+    apellido:apellido.toUpperCase(),
+    nota1:nota1,
+    nota2:nota2,
+    promedio:calcularPromedio () ,
+   }
+//Se agregan los estudiantes que se van cargando por lo inputs al array  mediante el método push
+   estudiantes.push(dataEstudiante);
     
 
+//La informacion se guarda en el localStorage
+    localStorage.setItem(id, JSON.stringify (dataEstudiante));
+
+
+//La información se envía a la base de datos, se utiliza asincronía y fetch
     
+    mostrar2.addEventListener ("click", ()=>{
+    fetch ('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        body:JSON.stringify(dataEstudiante),
+        headers:{
+        "Content-type":"application/json; charset=UTF-8",
+    }
+     })
+    .then ((response) => response.json())
+    .then ((data) => {
+        console.log(data);
+        datos.innerHTML =  `<p>${data.id}</p>
+                            <p>${data.nombre}</p>
+                            <p>${data.apellido}</p>
+                            <p>${data.nota1}</p>
+                            <p>${data.nota2}</p>
+                            <p>${data.promedio}</p>
+                            <p> Datos enviados exitosamente</p>`
+    })
+});
+    }
+ 
+  
+
 /*
 document.getElementById("mostrar").addEventListener ("click", function(){
 
@@ -94,5 +105,5 @@ document.getElementById("mostrar").addEventListener ("click", function(){
     }
 
 
-});
+})
 */
